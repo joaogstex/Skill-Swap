7e95ddf6d2581398f201556ff0d83d61aa1d3747
joaogstex <joaogstex@gmail.com>
2025-05-12 23:02:07 -0300

    Adding initial project

---
+++ src/test/java/br/com/teamss/skillswap/skill_swap/SkillSwapApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teamss.skillswap.skill_swap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSwapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
